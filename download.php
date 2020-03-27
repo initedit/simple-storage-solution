b33e2acb2ab6cda4f2e72f0763713a38a0060ea4
ashish maurya <ashishmaurya@outlook.com>
2020-03-27 11:42:31 +0530

    remove dependencies for query param `name`

--- download.php
+++ download.php
@@ -6,13 +6,18 @@ set_time_limit(0); // disable the time limit for this script
 $path = $config["upload_dir"]; // change the path to fit your websites document structure
 
 $dl_file = $_GET['file']; // simple file name validation
-$dl_file_name = $_GET['name']; // simple file name validation
 $fullPath = $path . $dl_file;
 if (file_exists($fullPath)) {
     if ($fd = fopen($fullPath, "r")) {
         $fsize = filesize($fullPath);
         $path_parts = pathinfo($fullPath);
         $ext = strtolower($path_parts["extension"]);
+
+        //get filename
+        $indexof = strpos($dl_file,$config["prefix_seperator"]);
+        $file_name = substr($dl_file,$indexof+1);
+        $dl_file_name = $file_name;
+
         switch ($ext) {
             case "pdf":
                 header("Content-type: application/pdf");
